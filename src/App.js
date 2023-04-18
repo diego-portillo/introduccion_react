61b426dcc70027f78631af8bb754e8eb23233e30
Diego Portillo <djportillo@taxit.com.py>
2023-04-18 12:23:22 -0400

    clase6:jsx-componentes-elementos-props-atributos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import logo from './logo.svg';
 import { ToDoCounter } from './ToDoCounter'
 import { ToDoSearcher } from "./ToDoSearcher";
 import { CreateToDoButton } from "./CreateToDoButton";
 import { ToDoList } from "./ToDoList";
 import { ToDoItem } from "./ToDoItem";
-// import './App.css';
+import './App.css';
 
 const toDos = [
   {text:'Cortar cebolla', completed: false},
@@ -12,19 +13,36 @@ const toDos = [
   {text:'Llorar con la llorona', completed: false}
 ]
 
-function App() {
+function App(props) {
   return (
-    <React.Fragment>
-    <ToDoCounter />
-    <ToDoSearcher/>
-    <ToDoList>
-      {toDos.map(todo => (
-        <ToDoItem key={todo.text} text={todo.text}/>
-      ))}
-      </ToDoList>
-   <CreateToDoButton/>
+  //   <React.Fragment>
+  //   <ToDoCounter />
+  //   <ToDoSearcher/>
+  //   <ToDoList>
+  //     {toDos.map(todo => (
+  //       <ToDoItem key={todo.text} text={todo.text}/>
+  //     ))}
+  //     </ToDoList>
+  //  <CreateToDoButton/>
     
-    </React.Fragment>
+  //   </React.Fragment>
+  <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {props.saludo}
+          {props.children}
+        </a>
+      </header>
+    </div>
   );
 }
 
